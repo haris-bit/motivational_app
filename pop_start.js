4d9df25c2408a91c3aa2c8f2c8baefd775f7903b
Haris Iftikhar <59412623+haris-bit@users.noreply.github.com>
2020-09-05 19:58:05 +0500

    Add files via upload

---
+++ pop_start.js
@@ -0,0 +1,24 @@
+const successURL = "https://giphy.com/gifs/workingtitlefilms-mr-bean-rowan-atkinson-beans-holiday-kfRlTZDvhLCPvOEey8";
+const failURL    = "https://giphy.com/gifs/fail-sad-pop-jaAXjJcaTngemp4vK8";
+const duration   = 10000;
+
+let balloons = [document.querySelector("#greenBalloon"),
+               document.querySelector("#redBalloon"),
+               document.querySelector("#yellowBalloon")]
+let numStopped = 0;
+
+balloons.forEach((balloon, index) => {
+    balloon.animation = balloon.animate([{top: "80vh"}, {top: "5vh"}], {duration: duration, fill: "forwards"});
+   	balloon.animation.onfinish = () => {
+   		window.location.href = failURL;
+  }
+  balloon.addEventListener("click", (e) => {
+
+  	e.currentTarget.animation.pause();
+  	numStopped++;
+  	if (numStopped>2){
+  		window.location.href = successURL;
+  	}
+  })
+
+});
